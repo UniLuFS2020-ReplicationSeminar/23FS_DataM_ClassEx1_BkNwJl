7ef344c11210221976ae6872bda6d3ee016527dd
‚jullatt‘ <julien.nicolas@bluewin.ch>
2023-03-11 11:14:36 +0100

    First preprocessing steps

--- scripts/ImportPreprocess.R
+++ scripts/ImportPreprocess.R
@@ -1,4 +1,16 @@
 ##########################################
 # Importing and Preprocessing data
 ##########################################
+install.packages("here")
+install.packages("rio")
+library(tidyverse)
+library(here)
+dat <- rio::import(here::here("data", "ESS1-7e01.csv"))
 
+# Preprocessing: Choosing Variables and Filtering Refusals
+
+dat1 <- dat %>% 
+  select(cntry, cseqno, tvpol, polintr, trstprl, gndr, agea, stfdem) %>% 
+  filter(cntry == "CH", tvpol != 66 & tvpol != 88)
+
+  
