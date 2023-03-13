45dc75ac5f12a90e9007ca516d9f72402ee05951
‚jullatt‘ <julien.nicolas@bluewin.ch>
2023-03-13 15:59:09 +0100

    first possible regression model

--- scripts/RegressionModel.R
+++ scripts/RegressionModel.R
@@ -1,4 +1,13 @@
+library(here)
 ##########################################
 # Regression Model
 ##########################################
 
+# import data
+ess_data <- readRDS(here("data", "data_processed.rds"))
+
+# modelling satisfaction with democracy
+model <- lm(stfdem ~ tvpol + polintr + trstprl + gndr + agea, ess_data)
+summary(model)
+
+# export model object
