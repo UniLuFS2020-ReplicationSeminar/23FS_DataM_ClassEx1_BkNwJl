4f282ff99edbe36767cfb3f87c6418632c0ca81f
‚jullatt‘ <julien.nicolas@bluewin.ch>
2023-03-13 14:37:01 +0100

    preprocessed data export

--- scripts/ImportPreprocess.R
+++ scripts/ImportPreprocess.R
@@ -1,16 +1,24 @@
 ##########################################
 # Importing and Preprocessing data
 ##########################################
-install.packages("here")
-install.packages("rio")
+# install.packages("here")
+# install.packages("rio")
 library(tidyverse)
 library(here)
-dat <- rio::import(here::here("data", "ESS1-7e01.csv"))
+library(rio)
+dat <- import(here("data", "ESS1-7e01.csv"))
 
-# Preprocessing: Choosing Variables and Filtering Refusals
+# Preprocessing: Choosing Variables and Filtering Refusals / Missing Answers
 
-dat1 <- dat %>% 
+dat2 <- dat %>% 
   select(cntry, cseqno, tvpol, polintr, trstprl, gndr, agea, stfdem) %>% 
-  filter(cntry == "CH", tvpol != 66 & tvpol != 88)
+  filter(cntry == "CH", 
+         tvpol != 66 & tvpol != 77 & tvpol != 88 & tvpol != 99,
+         polintr != 7 & polintr != 8 & polintr != 9,
+         trstprl != 77 & trstprl != 88 & trstprl != 99,
+         gndr != 9,
+         stfdem != 77 & stfdem != 88 & stfdem != 99)
 
-  
+# Storing processed data within data folder
+
+saveRDS(dat2, file = here("data", "data_processed.rds"))
