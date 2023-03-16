2cc7b55e2c5112326cd2280e3ce54f3f7dbccd2d
‚jullatt‘ <julien.nicolas@bluewin.ch>
2023-03-16 13:19:30 +0100

    suggest plots

--- scripts/MakePlot.R
+++ scripts/MakePlot.R
@@ -1,4 +1,28 @@
 ##########################################
 # Make Plot for Data
 ##########################################
+library(here)
+library(ggplot2)
+
+dat <- readRDS(here("data", "data_processed.rds")) # reading preprocessed data
+
+#Plotting possible relation of satiscation and tv usage
+ggplot(dat) +
+  geom_jitter(mapping = aes(x = tvpol, y = stfdem), color = "steelblue", shape = 7) +
+  geom_smooth(mapping = aes(x = tvpol, y = stfdem), color = "red4", method = "lm") +
+  labs(
+    title = "Satisfaction with democracy and TV usage",
+    x = "TV usage",
+    y = "Satisfaction")
+
+
+#Plotting possible relation of satiscation and political interest
+ggplot(dat) +
+  geom_jitter(mapping = aes(x = polintr, y = stfdem), color = "steelblue", shape = 7) +
+  geom_smooth(mapping = aes(x = polintr, y = stfdem), color = "red4", method = "lm") +
+  labs(
+    title = "Satisfaction with democracy and TV usage",
+    x = "Political interest",
+    y = "Satisfaction")
+  
 
