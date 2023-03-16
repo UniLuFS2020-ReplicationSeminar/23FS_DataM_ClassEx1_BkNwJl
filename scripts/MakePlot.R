46825b8b6aca20cf8c9696f1593ebfe2940a8520
nigi1992 <nicolas.waser@icloud.com>
2023-03-16 18:40:22 +0100

    - saving ggplot as images in image folder

--- scripts/MakePlot.R
+++ scripts/MakePlot.R
@@ -6,7 +6,7 @@ library(ggplot2)
 
 dat <- readRDS(here("data", "data_processed.rds")) # reading preprocessed data
 
-#Plotting possible relation of satiscation and tv usage
+#Plotting possible relation of satisfaction and TV usage
 ggplot1 <- ggplot(dat) +
   geom_jitter(mapping = aes(x = tvpol, y = stfdem), color = "steelblue", fill = NA, shape = 4, size = 1.2) +
   geom_smooth(mapping = aes(x = tvpol, y = stfdem), color = "red4", linetype = 1, linewidth = 0.5, method = "lm") +
@@ -16,11 +16,10 @@ ggplot1 <- ggplot(dat) +
     x = "TV usage",
     y = "Satisfaction") 
   
-save(ggplot, file = here("data", "ggplot1.RData")) #saving in R format
-save(ggplot, file = here("images", "ggplot1.png")) #saving as image
+save(ggplot, file = here("data", "ggplot1.RData")) #saving ggplot in R format
+ggsave(file = here("images", "Satisfaction-TVusage.png")) #saving as image
 
-
-#Plotting possible relation of satiscation and political interest
+#Plotting possible relation of satisfaction and political interest
 ggplot2 <- ggplot(dat) +
   geom_jitter(mapping = aes(x = polintr, y = stfdem), color = "steelblue", fill = NA, shape = 4, size = 1.2) +
   geom_smooth(mapping = aes(x = polintr, y = stfdem), color = "red4", linetype = 1, linewidth = 0.5, method = "lm") +
@@ -30,8 +29,7 @@ ggplot2 <- ggplot(dat) +
     x = "Political interest",
     y = "Satisfaction")
   
-save(ggplot, file = here("data", "ggplot2.RData")) #saving in R format
-save(ggplot, file = here("images", "ggplot2.png")) #saving as image
-
+save(ggplot, file = here("data", "ggplot2.RData")) #saving ggplot in R format
+ggsave(file = here("images", "Satisfaction-Pol.Interest.png")) #saving as image
 
 
