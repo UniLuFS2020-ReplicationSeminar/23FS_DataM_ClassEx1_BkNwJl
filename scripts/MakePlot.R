ce606fee134241c6b1e31da5d139361b6a8dc6ed
Benjamin Kurmann <benjamin@kurmail.ch>
2023-03-16 20:11:39 +0100

    adds a minimal version of ggplot2. just as suggestion. and to generate commits.

--- scripts/MakePlot.R
+++ scripts/MakePlot.R
@@ -32,4 +32,23 @@ ggplot2 <- ggplot(dat) +
 save(ggplot, file = here("data", "ggplot2.RData")) #saving ggplot in R format
 ggsave(file = here("images", "Satisfaction-Pol.Interest.png")) #saving as image
 
+#the above plot, but more «minimal» (suggestion commit)
+ggplot_minimal <- ggplot(dat) +
+  geom_jitter(mapping = aes(x = polintr, y = stfdem), color = "steelblue", fill = NA, shape = 4, size = 1.2, alpha = 0.7) +
+  geom_smooth(mapping = aes(x = polintr, y = stfdem), color = "red4", linetype = 1, linewidth = 1, method = "lm", se = FALSE) +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5, size = 20, face = "bold"),
+    axis.title.x = element_text(size = 14, face = "bold"),
+    axis.title.y = element_text(size = 14, face = "bold"),
+    axis.text.x = element_text(size = 12),
+    axis.text.y = element_text(size = 12),
+    panel.grid.minor = element_blank()
+  ) +
+  labs(
+    title = "Satisfaction with Democracy and TV Usage",
+    x = "Political Interest",
+    y = "Satisfaction"
+  )
 
+ggplot_minimal
